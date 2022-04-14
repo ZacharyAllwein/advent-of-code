53896dd8d3b1f252e53d39143c86a48608d7e2c1
Zachary Allwein <zrallwein@gmail.com>
2022-04-14 02:20:36 -0500

    lmao I only had to comment out a line of code and copy other parts for part 2

--- src/day5.rs
+++ src/day5.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 use std::io::prelude::*;
 
-pub fn part1<T: Read>(buf: std::io::BufReader<T>) {
+pub fn day5<T: Read>(buf: std::io::BufReader<T>) {
     //turn the file into usable data: [[(0, 1), (0,2)], etc..]
     let coordinate_lines = parse(buf);
 
@@ -9,7 +9,8 @@ pub fn part1<T: Read>(buf: std::io::BufReader<T>) {
     let coordinate_lines: Vec<Vec<(i32, i32)>> = coordinate_lines
         .into_iter()
         .map(|vec| [vec[0], vec[1]])
-        .filter(|[(x1, y1), (x2, y2), ..]| x1 == x2 || y1 == y2)
+        //uncomment below line for day5
+        // .filter(|[(x1, y1), (x2, y2), ..]| x1 == x2 || y1 == y2)
         .map(make_line)
         .collect();
 
@@ -44,7 +45,17 @@ fn parse<T: Read>(buf: std::io::BufReader<T>) -> Vec<Vec<(i32, i32)>> {
 fn make_line([(mut x1, mut y1), (mut x2, mut y2)]: [(i32, i32); 2]) -> Vec<(i32, i32)> {
     let mut line: Vec<(i32, i32)> = vec![];
 
-    if x2 - x1 != 0 {
+    if x2 - x1 != 0 && y2 - y1 != 0 {
+        let x_step = (x2 - x1) / (x2 - x1).abs();
+        let y_step = (y2 - y1) / (y2 - y1).abs();
+
+        while x1 != x2 {
+            line.push((x1, y1));
+            x1 += x_step;
+            y1 += y_step;
+        }
+        line.push((x1, y1));
+    } else if x2 - x1 != 0 {
         let x_step = (x2 - x1) / (x2 - x1).abs();
 
         while x1 != x2 {
